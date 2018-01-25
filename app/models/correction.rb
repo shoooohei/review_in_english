a2275842ff577bff147b8821c99095b037857706
shohei <shoheimoment@gmail.com>
2018-01-25 20:13:37 +0900

    add validation

--- app/models/correction.rb
+++ app/models/correction.rb
@@ -1,7 +1,19 @@
 class Correction < ApplicationRecord
-  validates :content, presence: true
-
   has_many :clikes, dependent: :destroy
   belongs_to :user
   belongs_to :review
+
+  validates :content, presence: true
+  validate :prevent_same_person
+
+  def prevent_same_person
+    if wether_correction == true
+      review_user_id = Review.find(review_id).id
+      if review_user_id == user_id
+        errors[:base] << "You can't correct by yourself"
+      end
+    end
+  end
+
+
 end
